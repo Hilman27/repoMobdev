b9390dcad1d420fd206f9699d66be3f563d136a6
triyono <yono.tri@gmail.com>
2019-12-04 17:01:07 +0700

    add floating button

--- lib/page/eventpage/detail.dart
+++ lib/page/eventpage/detail.dart
@@ -6,6 +6,10 @@ class SilverAppBarContoh extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add_box),
+      ),
       body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(

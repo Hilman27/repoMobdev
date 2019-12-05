4e8bc5baf182f7a7f01cfac57018da36af108599
triyono <yono.tri@gmail.com>
2019-12-05 12:57:05 +0700

    hapus folder page

--- lib/main_triyono.dart
+++ lib/main_triyono.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:religi_app/constant/style.dart';
-import 'package:religi_app/widget/_widgets.dart';
+
+import 'package:religi_app/pages/eventpage/detail.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,22 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Container(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ButtonTemplate(
-                    labelbutton: 'login',
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+        home: Scaffold(
+            body: Center(
+      child: Text('text'),
+    )));
   }
 }

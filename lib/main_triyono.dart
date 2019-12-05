076c781d2c4df10b71a1b7411960d68fb211831c
triyono <yono.tri@gmail.com>
2019-12-05 15:42:33 +0700

    penambahan rating

--- lib/main_triyono.dart
+++ lib/main_triyono.dart
@@ -7,10 +7,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            body: Center(
-      child: Text('text'),
-    )));
+    return MaterialApp(home: PageDetailEvent());
   }
 }

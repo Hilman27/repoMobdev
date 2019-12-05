6b06f36f085b12ce4092eda0bb176e4a5ef4f716
triyono <yono.tri@gmail.com>
2019-12-05 12:08:06 +0700

    perbaikan conflict

--- lib/main_triyono.dart
+++ lib/main_triyono.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'page/eventpage/detail.dart';
-=======
 import 'package:religi_app/constant/style.dart';
 import 'package:religi_app/widget/_widgets.dart';
->>>>>>> 3a37ade0e282b004589289ba650f363e2d6be56e
 
 void main() => runApp(MyApp());
 
@@ -12,9 +8,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
-      home: SilverAppBarContoh(),
-=======
       home: Scaffold(
         body: SafeArea(
           child: Container(
@@ -31,7 +24,6 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
->>>>>>> 3a37ade0e282b004589289ba650f363e2d6be56e
     );
   }
 }

5db3d7649f30d11df9f10650eabbf05dba1acd6c
Hilman27 <HilmanMocha@users.noreply.github.com>
2019-12-05 17:03:09 +0700

    Update _const.dart

--- lib/constant/_const.dart
+++ lib/constant/_const.dart
@@ -1,8 +1,7 @@
 export 'style.dart';
 export 'fontStyle.dart';
 export 'size.dart';
-<<<<<<< lib/constant/_const.dart
 export 'color.dart';
 export 'style.dart';
 export 'package:division/division.dart';
->>>>>>> lib/constant/_const.dart
+

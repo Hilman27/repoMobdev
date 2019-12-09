bcdb3edb0771dd9c40d5f9bf5360208300b5ba24
triyono <yono.tri@gmail.com>
2019-12-09 15:42:20 +0700

    perbaikan gambar

--- lib/pages/eventpage/create2.dart
+++ lib/pages/eventpage/create2.dart
@@ -1,32 +1,20 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_tagging/flutter_tagging.dart';
 
 import 'dart:async';
 import 'package:flutter_syntax_view/flutter_syntax_view.dart';
 import 'package:religi_app/widget/_widgets.dart';
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Create event',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: TagKateori(),
-    );
-  }
-}
+import 'package:image_picker/image_picker.dart';
 
 ///
-class TagKateori extends StatefulWidget {
+class CreateEventPage extends StatefulWidget {
   @override
-  _TagKateoriState createState() => _TagKateoriState();
+  _CreateEventPageState createState() => _CreateEventPageState();
 }
 
-class _TagKateoriState extends State<TagKateori> {
+class _CreateEventPageState extends State<CreateEventPage> {
   // String _selectedValuesJson = "Nothing to show";
   List<Language> _selectedLanguages;
 
@@ -54,9 +42,11 @@ class _TagKateoriState extends State<TagKateori> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
+            PilihImage(),
             FormTextBiasa(
               namaLabel: 'judul',
             ),
+
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: FlutterTagging<Language>(
@@ -193,4 +183,107 @@ class Language extends Taggable {
     "name": $name,\n
     "position": $position\n
   }''';
+}
+
+// start ############# pilih Image ##############
+class PilihImage extends StatefulWidget {
+  @override
+  _PilihImageState createState() => _PilihImageState();
+}
+
+class _PilihImageState extends State<PilihImage> {
+  File _imageFile;
+  dynamic _pickImageError;
+  String _retrieveDataError;
+
+  Future getImageCamera() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _imageFile = image;
+    });
+  }
+
+  Future getImageGallery() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _imageFile = image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  // width: 200,
+                  child: _imageFile == null
+                      ? Container(
+                          color: Colors.green,
+                          width: MediaQuery.of(context).size.width,
+                          height: 200,
+                          child: Center(child: Text('No image selected.')),
+                        )
+                      : Image.file(_imageFile),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: getImageCamera,
+                      tooltip: 'pilih foto',
+                      icon: Icon(Icons.add_a_photo),
+                    ),
+                    IconButton(
+                      onPressed: getImageGallery,
+                      tooltip: 'pilih foto',
+                      icon: Icon(Icons.image),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _previewImage() {
+    final Text retrieveError = _getRetrieveErrorWidget();
+    if (retrieveError != null) {
+      return retrieveError;
+    }
+    if (_imageFile != null) {
+      return Image.file(_imageFile);
+    } else if (_pickImageError != null) {
+      return Text(
+        'Pick image error: $_pickImageError',
+        textAlign: TextAlign.center,
+      );
+    } else {
+      return const Text(
+        'You have not yet picked an image.',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+  Text _getRetrieveErrorWidget() {
+    if (_retrieveDataError != null) {
+      final Text result = Text(_retrieveDataError);
+      _retrieveDataError = null;
+      return result;
+    }
+    return null;
+  }
 }

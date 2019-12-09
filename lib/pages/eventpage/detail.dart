0b3b22afae26d1b1a520d10497f56c2d666c39be
triyono <yono.tri@gmail.com>
2019-12-09 12:37:14 +0700

    penambahan library photo view dan update gallery

--- lib/pages/eventpage/detail.dart
+++ lib/pages/eventpage/detail.dart
@@ -1,12 +1,21 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 import 'package:religi_app/widget/_widgets.dart';
+
 // import 'package:religi_app/widgets/textform.da5rt';
 
 // import 'package:religi_app/widget/_widgets.dart';
 
-class PageDetailEvent extends StatelessWidget {
+class PageDetailEvent extends StatefulWidget {
+  @override
+  _PageDetailEventState createState() => _PageDetailEventState();
+}
+
+class _PageDetailEventState extends State<PageDetailEvent> {
+  bool verticalGallery = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -88,28 +97,68 @@ class PageDetailEvent extends StatelessWidget {
                         padding: const EdgeInsets.symmetric(horizontal: 8),
                         child: Row(
                           children: <Widget>[
-                            new GambarContainer(),
-                            new Sizedbox5(),
-                            new GambarContainer(),
-                            new Sizedbox5(),
-                            new GambarContainer(),
-                            new Sizedbox5(),
-                            new GambarContainer(),
-                            new Sizedbox5(),
-                            new GambarContainer(),
-                            new Sizedbox5(),
-                            new GambarContainer(),
-                            new Sizedbox5(),
-                            new GambarContainer(),
-                            new Sizedbox5(),
-                            new GambarContainer(),
-                            new Sizedbox5(),
-                            new GambarContainer(),
-                            new Sizedbox5(),
-                            new GambarContainer(),
-                            new Sizedbox5(),
-                            new GambarContainer(),
-                            new Sizedbox5(),
+                            // new GambarContainer(),
+                            // new Sizedbox5(),
+                            // new GambarContainer(),
+                            // new Sizedbox5(),
+                            // new GambarContainer(),
+                            // new Sizedbox5(),
+                            // new GambarContainer(),
+                            // new Sizedbox5(),
+                            // new GambarContainer(),
+                            // new Sizedbox5(),
+                            // new GambarContainer(),
+                            // new Sizedbox5(),
+                            // new GambarContainer(),
+                            // new Sizedbox5(),
+                            GalleryExampleItemThumbnail(
+                              galleryExampleItem: galleryItems[0],
+                              onTap: () {
+                                open(context, 0);
+                              },
+                            ),
+                            GalleryExampleItemThumbnail(
+                              galleryExampleItem: galleryItems[2],
+                              onTap: () {
+                                open(context, 2);
+                              },
+                            ),
+                            GalleryExampleItemThumbnail(
+                              galleryExampleItem: galleryItems[3],
+                              onTap: () {
+                                open(context, 3);
+                              },
+                            ),
+                            GalleryExampleItemThumbnail(
+                              galleryExampleItem: galleryItems[4],
+                              onTap: () {
+                                open(context, 4);
+                              },
+                            ),
+                            GalleryExampleItemThumbnail(
+                              galleryExampleItem: galleryItems[5],
+                              onTap: () {
+                                open(context, 5);
+                              },
+                            ),
+                            GalleryExampleItemThumbnail(
+                              galleryExampleItem: galleryItems[6],
+                              onTap: () {
+                                open(context, 6);
+                              },
+                            ),
+                            GalleryExampleItemThumbnail(
+                              galleryExampleItem: galleryItems[7],
+                              onTap: () {
+                                open(context, 7);
+                              },
+                            ),
+                            GalleryExampleItemThumbnail(
+                              galleryExampleItem: galleryItems[1],
+                              onTap: () {
+                                open(context, 1);
+                              },
+                            ),
                           ],
                         ),
                       ),
@@ -150,6 +199,22 @@ class PageDetailEvent extends StatelessWidget {
           ),
         ),
       );
+
+  void open(BuildContext context, final int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GalleryPhotoViewWrapper(
+          galleryItems: galleryItems,
+          backgroundDecoration: const BoxDecoration(
+            color: Colors.black,
+          ),
+          initialIndex: index,
+          scrollDirection: verticalGallery ? Axis.vertical : Axis.horizontal,
+        ),
+      ),
+    );
+  }
 }
 
 class GambarContainer extends StatelessWidget {
@@ -359,5 +424,201 @@ class SizedBox10 extends StatelessWidget {
     return SizedBox(
       height: 10,
     );
+  }
+}
+
+class GalleryExampleItem {
+  GalleryExampleItem({this.id, this.resource, this.isSvg = false});
+
+  final String id;
+  final String resource;
+  final bool isSvg;
+}
+
+class GalleryExampleItemThumbnail extends StatelessWidget {
+  const GalleryExampleItemThumbnail(
+      {Key key, this.galleryExampleItem, this.onTap})
+      : super(key: key);
+
+  final GalleryExampleItem galleryExampleItem;
+
+  final GestureTapCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Hero(
+          tag: galleryExampleItem.id,
+          child: Image.asset(galleryExampleItem.resource, height: 200.0),
+        ),
+      ),
+    );
+  }
+}
+
+List<GalleryExampleItem> galleryItems = <GalleryExampleItem>[
+  GalleryExampleItem(
+    id: "tag1",
+    resource: "assets/images/profil.png",
+  ),
+  GalleryExampleItem(id: "tag2", resource: "assets/images/profil.png"),
+  GalleryExampleItem(
+    id: "tag3",
+    resource: "assets/images/profil.png",
+  ),
+  GalleryExampleItem(
+    id: "tag5",
+    resource: "assets/images/profil.png",
+  ),
+  GalleryExampleItem(
+    id: "tag6",
+    resource: "assets/images/profil.png",
+  ),
+  GalleryExampleItem(
+    id: "tag7",
+    resource: "assets/images/profil.png",
+  ),
+  GalleryExampleItem(
+    id: "tag8",
+    resource: "assets/images/profil.png",
+  ),
+  GalleryExampleItem(
+    id: "tag9",
+    resource: "assets/images/profil.png",
+  ),
+];
+
+class GalleryGambar extends StatefulWidget {
+  @override
+  _GalleryGambarState createState() => _GalleryGambarState();
+}
+
+class _GalleryGambarState extends State<GalleryGambar> {
+  bool verticalGallery = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  void open(BuildContext context, final int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GalleryPhotoViewWrapper(
+          galleryItems: galleryItems,
+          backgroundDecoration: const BoxDecoration(
+            color: Colors.black,
+          ),
+          initialIndex: index,
+          scrollDirection: verticalGallery ? Axis.vertical : Axis.horizontal,
+        ),
+      ),
+    );
+  }
+}
+
+class GalleryPhotoViewWrapper extends StatefulWidget {
+  GalleryPhotoViewWrapper({
+    this.loadingChild,
+    this.backgroundDecoration,
+    this.minScale,
+    this.maxScale,
+    this.initialIndex,
+    @required this.galleryItems,
+    this.scrollDirection = Axis.horizontal,
+  }) : pageController = PageController(initialPage: initialIndex);
+
+  final Widget loadingChild;
+  final Decoration backgroundDecoration;
+  final dynamic minScale;
+  final dynamic maxScale;
+  final int initialIndex;
+  final PageController pageController;
+  final List<GalleryExampleItem> galleryItems;
+  final Axis scrollDirection;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _GalleryPhotoViewWrapperState();
+  }
+}
+
+class _GalleryPhotoViewWrapperState extends State<GalleryPhotoViewWrapper> {
+  int currentIndex;
+
+  @override
+  void initState() {
+    currentIndex = widget.initialIndex;
+    super.initState();
+  }
+
+  void onPageChanged(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: widget.backgroundDecoration,
+        constraints: BoxConstraints.expand(
+          height: MediaQuery.of(context).size.height,
+        ),
+        child: Stack(
+          alignment: Alignment.bottomRight,
+          children: <Widget>[
+            PhotoViewGallery.builder(
+              scrollPhysics: const BouncingScrollPhysics(),
+              builder: _buildItem,
+              itemCount: galleryItems.length,
+              loadingChild: widget.loadingChild,
+              backgroundDecoration: widget.backgroundDecoration,
+              pageController: widget.pageController,
+              onPageChanged: onPageChanged,
+              scrollDirection: widget.scrollDirection,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "Image ${currentIndex + 1}",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 17.0,
+                  decoration: null,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  PhotoViewGalleryPageOptions _buildItem(BuildContext context, int index) {
+    final GalleryExampleItem item = widget.galleryItems[index];
+    return item.isSvg
+        ? PhotoViewGalleryPageOptions.customChild(
+            child: Container(
+              width: 300,
+              height: 300,
+            ),
+            childSize: const Size(300, 300),
+            initialScale: PhotoViewComputedScale.contained,
+            minScale: PhotoViewComputedScale.contained * (0.5 + index / 10),
+            maxScale: PhotoViewComputedScale.covered * 1.1,
+            heroAttributes: PhotoViewHeroAttributes(tag: item.id),
+          )
+        : PhotoViewGalleryPageOptions(
+            imageProvider: AssetImage(item.resource),
+            initialScale: PhotoViewComputedScale.contained,
+            minScale: PhotoViewComputedScale.contained * (0.5 + index / 10),
+            maxScale: PhotoViewComputedScale.covered * 1.1,
+            heroAttributes: PhotoViewHeroAttributes(tag: item.id),
+          );
   }
 }
